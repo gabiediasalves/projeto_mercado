aa636e05e5040411aae7b2842fb7ca55ac25aa80
Gabrieli Dias Alves <95694730+gabiediasalves@users.noreply.github.com>
2023-01-17 17:49:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function mostra_template_home() {
     carouselExampleIndicators.style.display = 'block'
     template_relatorio.style.display = 'none'
     template_venda.style.display = 'none'
+    template_sacola.style.display = 'none'
     template_cadastro.style.display = 'none'
 }
-
 
 function mostra_template_cadastro() {
     if (template_cadastro.style.display === 'block') {
@@ -18,6 +18,7 @@ function mostra_template_cadastro() {
     } else {
         template_cadastro.style.display = 'block'
         template_relatorio.style.display = 'none'
+        template_sacola.style.display = 'none'
         template_venda.style.display = 'none'
         carouselExampleIndicators.style.display = 'none'
 
@@ -32,6 +33,7 @@ function mostra_template_relatorio() {
     } else {
         template_relatorio.style.display = 'block'
         template_cadastro.style.display = 'none'
+        template_sacola.style.display = 'none'
         template_venda.style.display = 'none'
         carouselExampleIndicators.style.display = 'none'
 
@@ -47,6 +49,20 @@ function mostra_template_venda() {
         template_venda.style.display = 'block'
         template_relatorio.style.display = 'none'
         template_cadastro.style.display = 'none'
+        template_sacola.style.display = 'none'
+        carouselExampleIndicators.style.display = 'none'
+    }
+}
+var template_sacola = document.querySelector('.template_sacola')
+function mostra_template_sacola () {
+    if (template_sacola.style.display === 'block') {
+        template_sacola.style.display = 'none'
+        carouselExampleIndicators.style.display = 'block'
+    } else {
+        template_sacola.style.display = 'block'
+        template_relatorio.style.display = 'none'
+        template_cadastro.style.display = 'none'
+        template_venda.style.display = 'none'
         carouselExampleIndicators.style.display = 'none'
     }
 }
@@ -62,7 +78,7 @@ function cadastrar() {
         alert("Código inválido!")
 
     } else if (valida_input(lista[lista.length - 1][0]) && valida_input(lista[lista.length - 1][1]) && valida_input(lista[lista.length - 1][2]) && valida_input(lista[lista.length - 1][3])) {
-
+        alert("Produto cadastrado com sucesso!")
         var table = document.querySelector('.table')
         var row = table.insertRow(1)
         var cell1 = row.insertCell(0)
@@ -80,6 +96,7 @@ function cadastrar() {
         option.value = `${lista[lista.length - 1][0]}`
         x.add(option)
     }
+
 }
 
 var valida_input = (s) => {
@@ -131,19 +148,29 @@ function venda() {
             if (estoque_venda <= lista[i][3] && estoque_venda > 0) {
                 alert("Produto adicionado na sacola!")
 
-                sacola.push(lista[i][0], lista[i][1], lista[i][2], lista[i][3], estoque_venda * lista[i][2])
+                sacola.push([lista[i][0], lista[i][1], lista[i][2], lista[i][3], estoque_venda * lista[i][2]])
                 lista[i][3] = lista[i][3] - estoque_venda
 
+                console.log(sacola)
+                var table_t = document.querySelector('.table_venda')
 
-                var table = document.querySelector('.table_venda')
-                var row = table.insertRow(1)
+                var row = table_t.insertRow(1)
                 var cell1 = row.insertCell(0)
                 var cell2 = row.insertCell(1)
                 var cell3 = row.insertCell(2)
-        
-                cell1.innerHTML = `${lista[lista.length - 1][1]}`
+                cell1.innerHTML = `Qnt.${estoque_venda}`
+                cell2.innerHTML = `Uni ${sacola[i][2]}`
+                cell3.innerHTML = `Total${sacola[i][4]}`
+
+                var row = table_t.insertRow(1)
+                var cell1 = row.insertCell(0)
+                var cell2 = row.insertCell(1)
+                var cell3 = row.insertCell(2)
+                cell1.innerHTML = `Produto ${sacola[sacola.length - 1][1]}`
                 cell2.innerHTML = ``
                 cell3.innerHTML = ``
+
+
 
 
             } else {
